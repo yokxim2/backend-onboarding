57d1ca4533e309ef3e5de355b62c622661dc7d6c
yokxim <yooksi53@gmail.com>
2025-02-13 19:04:00 +0900

    fix[user-service] Access 토큰 재발급 로직 추가
    - 재발급 요청 시 Refresh 토큰 확인
    - Access 토큰을 재 발급 할 때 Refresh 토큰 재사용 방지를 위해 Refresh 토큰도 같이 재발급

--- src/main/java/com/example/backendonboarding/service/UserService.java
+++ src/main/java/com/example/backendonboarding/service/UserService.java
@@ -2,20 +2,33 @@ package com.example.backendonboarding.service;
 
 import com.example.backendonboarding.dto.SignupRequestDto;
 import com.example.backendonboarding.dto.SignupResponseDto;
+import com.example.backendonboarding.entity.Refresh;
 import com.example.backendonboarding.entity.User;
+import com.example.backendonboarding.jwt.JwtUtil;
+import com.example.backendonboarding.repository.RefreshRepository;
 import com.example.backendonboarding.repository.UserRepository;
+import jakarta.servlet.http.Cookie;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+import static com.example.backendonboarding.jwt.LoginFilter.ACCESS_TOKEN_EXPIRATION_TIME;
+import static com.example.backendonboarding.jwt.LoginFilter.REFRESH_TOKEN_EXPIRATION_TIME;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, JwtUtil jwtUtil, RefreshRepository refreshRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+        this.refreshRepository = refreshRepository;
     }
 
     public SignupResponseDto signup(SignupRequestDto requestDto) {
@@ -38,5 +51,57 @@ public class UserService {
 
         User savedUser = userRepository.save(newUser);
         return new SignupResponseDto(savedUser);
+    }
+
+    public String[] reissue(Cookie[] cookies) {
+
+        String refresh = null;
+        String[] tokens = new String[2];
+
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("refresh")) {
+                refresh = cookie.getValue();
+            }
+        }
+
+        if (refresh == null) {
+            throw new IllegalArgumentException("Refresh cookie not found");
+        }
+
+        String category = jwtUtil.getCategory(refresh);
+
+        if (!category.equals("refresh")) {
+            throw new IllegalArgumentException("Invalid refresh cookie");
+        }
+
+        Boolean isExist = refreshRepository.existsByRefresh(refresh);
+        if (!isExist) {
+            throw new IllegalArgumentException("Refresh cookie not found");
+        }
+
+        String username = jwtUtil.getUsername(refresh);
+        String role = jwtUtil.getRole(refresh);
+
+        String newAccess = jwtUtil.createJwt("access", username, role, ACCESS_TOKEN_EXPIRATION_TIME);
+        String newRefresh = jwtUtil.createJwt("refresh", username, role, REFRESH_TOKEN_EXPIRATION_TIME);
+
+        refreshRepository.deleteByRefresh(refresh);
+        saveNewRefresh(username, newRefresh, REFRESH_TOKEN_EXPIRATION_TIME);
+
+        tokens[0] = newAccess;
+        tokens[1] = newRefresh;
+
+        return tokens;
+    }
+
+    private void saveNewRefresh(String username, String newRefresh, long expiredMs) {
+        Date date = new Date(System.currentTimeMillis() + expiredMs);
+
+        Refresh refresh = new Refresh();
+        refresh.setUsername(username);
+        refresh.setRefresh(newRefresh);
+        refresh.setExpiration(date.toString());
+
+        refreshRepository.save(refresh);
     }
 }
