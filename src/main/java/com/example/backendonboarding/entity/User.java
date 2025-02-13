08dab14ab543eca882be727b4d512ee85ccca009
yokxim <yooksi53@gmail.com>
2025-02-13 14:15:31 +0900

    fix[user] User 엔티티 getter setter 추가

--- src/main/java/com/example/backendonboarding/entity/User.java
+++ src/main/java/com/example/backendonboarding/entity/User.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import javax.annotation.processing.Generated;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class User {
 
     @Id
