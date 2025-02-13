8442b796303ffea9fb04df5cd3ad1eee4122fdda
yokxim <yooksi53@gmail.com>
2025-02-13 16:21:39 +0900

    fix[jwt-filter] JWT 검증 로직 추가
    - access 토큰을 꺼내서 검증
    - 토큰에서 유저 정보를 꺼내서 유저 객체 생성 및 로그인 필터로 보냄

--- src/main/java/com/example/backendonboarding/jwt/JwtFilter.java
+++ src/main/java/com/example/backendonboarding/jwt/JwtFilter.java
@@ -1,12 +1,19 @@
 package com.example.backendonboarding.jwt;
 
+import com.example.backendonboarding.dto.CustomUserDetails;
+import com.example.backendonboarding.entity.User;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class JwtFilter extends OncePerRequestFilter {
 
@@ -18,6 +25,44 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = request.getHeader("access");
 
+        if (accessToken == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            jwtUtil.isExpired(accessToken);
+        } catch (ExpiredJwtException e) {
+            PrintWriter writer = response.getWriter();
+            writer.print("access token expired");
+
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String category = jwtUtil.getCategory(accessToken);
+
+        if (!category.equals("access")) {
+            PrintWriter writer = response.getWriter();
+            writer.print("Invalid access token");
+
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String username = jwtUtil.getUsername(accessToken);
+        String role= jwtUtil.getRole(accessToken);
+
+        User user = new User();
+        user.setUsername(username);
+        user.setRole(role);
+        CustomUserDetails userDetails = new CustomUserDetails(user);
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
     }
 }
