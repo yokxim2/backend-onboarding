9852ceef6ad2bd24ec626d1fff2266f352569c2d
yokxim <yooksi53@gmail.com>
2025-02-13 16:12:53 +0900

    fix[login-filter] 로그인 성공 시 분기 로직 추가
    - access 토큰은 헤더에 담음 (CSRF 공격 방지)
    - refresh 토큰은 쿠키에 담음 (XSS 공격 방지)

--- src/main/java/com/example/backendonboarding/jwt/LoginFilter.java
+++ src/main/java/com/example/backendonboarding/jwt/LoginFilter.java
@@ -1,18 +1,28 @@
 package com.example.backendonboarding.jwt;
 
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
+
+    public static long ACCESS_TOKEN_EXPIRATION_TIME = 600000L;
+    public static long REFRESH_TOKEN_EXPIRATION_TIME = 86400000L;
+    public static int COOKIE_MAX_AGE = 24*60*60;
 
     public LoginFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
         this.authenticationManager = authenticationManager;
@@ -32,7 +42,27 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+        String username = authentication.getName();
 
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        String access = jwtUtil.createJwt("access", username, role, ACCESS_TOKEN_EXPIRATION_TIME);
+        String refresh = jwtUtil.createJwt("refresh", username, role, REFRESH_TOKEN_EXPIRATION_TIME);
+
+        response.setHeader("access", access);
+        response.addCookie(createCookie("refresh", refresh));
+        response.setStatus(HttpStatus.OK.value());
+    }
+
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(COOKIE_MAX_AGE);
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 
     @Override
