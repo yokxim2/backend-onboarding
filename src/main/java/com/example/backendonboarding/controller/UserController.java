19465a99af18dbf0d6a6f080d48316d4b4c0a997
yokxim <yooksi53@gmail.com>
2025-02-13 19:24:56 +0900

    fix[user-controller] /reissue 주소로 접근할 때 Access 토큰을 재발급 하도록 함. 동시에 Refresh 토큰도 같이 재발급

--- src/main/java/com/example/backendonboarding/controller/UserController.java
+++ src/main/java/com/example/backendonboarding/controller/UserController.java
@@ -3,10 +3,15 @@ package com.example.backendonboarding.controller;
 import com.example.backendonboarding.dto.SignupRequestDto;
 import com.example.backendonboarding.dto.SignupResponseDto;
 import com.example.backendonboarding.service.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.example.backendonboarding.jwt.LoginFilter.COOKIE_MAX_AGE;
 
 @RestController
 public class UserController {
@@ -20,5 +25,26 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseEntity<SignupResponseDto> signup(SignupRequestDto requestDto) {
         return new ResponseEntity<>(userService.signup(requestDto), HttpStatus.OK);
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
+
+        Cookie[] cookies = request.getCookies();
+        String[] tokens = userService.reissue(cookies);
+
+        response.setHeader("access", tokens[0]);
+        response.addCookie(createCookie("refresh", tokens[1]));
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(COOKIE_MAX_AGE);
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }
