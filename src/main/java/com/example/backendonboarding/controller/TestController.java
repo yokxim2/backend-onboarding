9b4fef561bf334f98dd02c220e7f5028572ecad4
yokxim <yooksi53@gmail.com>
2025-02-14 02:25:36 +0900

    fix[test-controller] swagger UI에 보이지 않도록 @Hidden 추가

--- src/main/java/com/example/backendonboarding/controller/TestController.java
+++ src/main/java/com/example/backendonboarding/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.example.backendonboarding.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RestController
 @RequestMapping("/secured-endpoint")
 public class TestController {
