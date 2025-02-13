503efa423a511fa9cca56954aee5545bed61c416
yokxim <yooksi53@gmail.com>
2025-02-13 15:54:56 +0900

    feat[jwt-filter] JWT 토큰을 검증하는 필터 클래스 추가

---
+++ src/main/java/com/example/backendonboarding/jwt/JwtFilter.java
@@ -0,0 +1,23 @@
+package com.example.backendonboarding.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
