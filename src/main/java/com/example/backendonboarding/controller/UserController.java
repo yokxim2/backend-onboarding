719adb942aaf69cab0f023e1388cd5521c32657e
yokxim <yooksi53@gmail.com>
2025-02-13 19:45:23 +0900

    fix[user-controller] RequestBody로 JSON 입력값 받도록 수정

--- src/main/java/com/example/backendonboarding/controller/UserController.java
+++ src/main/java/com/example/backendonboarding/controller/UserController.java
@@ -8,12 +8,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import static com.example.backendonboarding.jwt.LoginFilter.COOKIE_MAX_AGE;
 
-@RestController
+@Controller
 public class UserController {
 
     private final UserService userService;
@@ -23,7 +25,7 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<SignupResponseDto> signup(SignupRequestDto requestDto) {
+    public ResponseEntity<SignupResponseDto> signup(@RequestBody SignupRequestDto requestDto) {
         return new ResponseEntity<>(userService.signup(requestDto), HttpStatus.OK);
     }
 
