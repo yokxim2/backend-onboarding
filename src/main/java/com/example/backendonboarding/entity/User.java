b7084ca61a1eabf224c784fda03f527f265b6cb3
yokxim <yooksi53@gmail.com>
2025-02-14 02:24:49 +0900

    fix[user] Username 값에 Unique 옵션 추가

--- src/main/java/com/example/backendonboarding/entity/User.java
+++ src/main/java/com/example/backendonboarding/entity/User.java
@@ -1,9 +1,6 @@
 package com.example.backendonboarding.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +13,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(nullable = false, unique = true)
     private String username;
     private String password;
     private String nickname;
