7f1bd9863d7c2f698349159c0f283190b4486c62
yokxim <yooksi53@gmail.com>
2025-02-13 14:15:59 +0900

    fix[user-repository] username으로 이미 등록된 유저 찾는 메서드 추가

--- src/main/java/com/example/backendonboarding/repository/UserRepository.java
+++ src/main/java/com/example/backendonboarding/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.backendonboarding.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean findByUsername(String username);
 }
