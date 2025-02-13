87daaa3bb2b7fa917310d797d527f46c23c02cde
yokxim <yooksi53@gmail.com>
2025-02-13 13:23:26 +0900

    Initial commit

---
+++ src/main/java/com/example/backendonboarding/BackendOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendOnboardingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendonboarding/BackendOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOnboardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
