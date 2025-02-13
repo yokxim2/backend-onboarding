2f574b90d9d19223268a7261a5730d20cc965991
yokxim <yooksi53@gmail.com>
2025-02-13 20:58:59 +0900

    fix[user-controller] 안쓰는 import 삭제

--- src/main/java/com/example/backendonboarding/controller/UserController.java
+++ src/main/java/com/example/backendonboarding/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import static com.example.backendonboarding.jwt.LoginFilter.COOKIE_MAX_AGE;
 
