108f054ec2e9efaa8d4ec6cd01b681657f707338
yokxim <yooksi53@gmail.com>
2025-02-14 02:28:24 +0900

    fix[signup-response-dto] Swagger UI 전용 설정 추가

--- src/main/java/com/example/backendonboarding/dto/SignupResponseDto.java
+++ src/main/java/com/example/backendonboarding/dto/SignupResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.backendonboarding.dto;
 
 import com.example.backendonboarding.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
@@ -10,9 +11,16 @@ import java.util.Collection;
 
 @Getter
 @Setter
+@Schema(description = "회원가입 응답 DTO")
 public class SignupResponseDto {
+
+    @Schema(description = "사용자 ID", example = "user123")
     private String username;
+
+    @Schema(description = "닉네임", example = "홍길동")
     private String nickname;
+
+    @Schema(description = "사용자 권한 목록")
     private Collection<? extends GrantedAuthority> authorities;
 
     public SignupResponseDto(User user) {
