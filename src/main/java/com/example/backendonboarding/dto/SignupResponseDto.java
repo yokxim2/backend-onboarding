f5d8c784511336f8cf7e7589235c2092857f1a7b
yokxim <yooksi53@gmail.com>
2025-02-13 19:45:54 +0900

    fix[signup-response-dto] 응답 값 타입 변경

--- src/main/java/com/example/backendonboarding/dto/SignupResponseDto.java
+++ src/main/java/com/example/backendonboarding/dto/SignupResponseDto.java
@@ -3,17 +3,22 @@ package com.example.backendonboarding.dto;
 import com.example.backendonboarding.entity.User;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Getter
 @Setter
 public class SignupResponseDto {
     private String username;
     private String nickname;
-    private String role;
+    private Collection<? extends GrantedAuthority> authorities;
 
     public SignupResponseDto(User user) {
         this.username = user.getUsername();
         this.nickname = user.getNickname();
-        this.role = user.getRole();
+        UserDetails userDetails = new CustomUserDetails(user);
+        this.authorities = userDetails.getAuthorities();
     }
 }
